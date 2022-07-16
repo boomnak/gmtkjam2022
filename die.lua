3a41ee2fcf433b81b27701407f17ce21e61fa0d3
Adam Vandolder <adam.vandolder@gmail.com>
2022-07-16 13:45:21 -0400

    Move die to separate module

---
+++ die.lua
@@ -0,0 +1,117 @@
+local lg = love.graphics
+
+local Die = {}
+
+function Die.new(x, y)
+  -- Load the die faces when the first die is created
+  if Die.faces == nil then
+    Die.faces = {}
+    for i = 1, 6 do
+      Die.faces[i] = lg.newImage("assets/img/" .. tostring(i) .. ".png")
+    end
+  end
+  if Die.rollSound == nil then
+    Die.rollSound = love.audio.newSource("assets/snd/dice.wav", "static")
+  end
+
+  return setmetatable({
+    x = x,
+    y = y,
+    w = 64,
+    h = 64,
+    top = 1,
+    right = 3,
+    down = 2,
+    steps = 0,
+  },
+    { __index = Die }
+  )
+end
+
+function Die:update(dt, map, isPressed)
+  local moved = false
+  local prev = {
+    x = self.x,
+    y = self.y,
+    top = self.top,
+    right = self.right,
+    down = self.down
+  }
+
+  if isPressed["w"] or isPressed["up"] then
+    self.y = self.y - 1
+    moved = true
+
+    local down = 7 - self.top
+    self.top = self.down
+    self.down = down
+  elseif isPressed["a"] or isPressed["left"] then
+    self.x = self.x - 1
+    moved = true
+
+    local right = 7 - self.top
+    self.top = self.right
+    self.right = right
+  elseif isPressed["s"] or isPressed["down"] then
+    self.y = self.y + 1
+    moved = true
+
+    local top = 7 - self.down
+    self.down = self.top
+    self.top = top
+  elseif isPressed["d"] or isPressed["right"] then
+    self.x = self.x + 1
+    moved = true
+
+    local top = 7 - self.right
+    self.right = self.top
+    self.top = top
+  end
+
+  if self.y <= 0 or self.y > #map
+      or self.x <= 0 or self.x > #map[1]
+      or map[self.y][self.x] == -1 then
+    for k, v in pairs(prev) do
+      self[k] = v
+    end
+    moved = false
+  end
+  if moved then
+    self.steps = self.steps + 1
+    love.audio.stop(Die.rollSound)
+    love.audio.play(Die.rollSound)
+  end
+end
+
+function Die:draw()
+  lg.setColor(1, 1, 1, 1)
+  lg.draw(
+    Die.faces[self.top],
+    (self.x - 1) * self.w - self.w / 2,
+    (self.y - 1) * self.h - self.h / 2
+  )
+
+  lg.setColor(.85, .85, .85, 1)
+  lg.draw(
+    Die.faces[self.down],
+    (self.x - 1) * self.w - self.w / 2,
+    (self.y - 1) * self.h + self.h / 2,
+    nil,
+    nil, 0.5,
+    nil, nil,
+    0.5, 0
+  )
+
+  lg.setColor(.75, .75, .75, 1)
+  lg.draw(
+    Die.faces[self.right],
+    self.x * self.w - 16,
+    self.y * self.h - 48,
+    -math.pi / 2,
+    -1, 0.5,
+    32, 32,
+    0.5, 0
+  )
+end
+
+return Die
--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+local Die = require "die"
+
 local lg = love.graphics
 local lk = love.keyboard
 
@@ -8,50 +10,26 @@ local map = {
   { -1, -1, 0, 0, 0 },
   { 0, 2, 0, -1, -1 },
 }
-
-local mapx, mapy
 local tileSize = 64
-
-local die = {
-  x = 3,
-  y = 3,
-  w = 64,
-  h = 64,
-  top = 1,
-  right = 3,
-  down = 2,
-  steps = 0,
-}
-local dieRollSound
-local dieFaces = {}
-
+local mapTransform
+local die = Die.new(3, 3)
 local objectives = {}
-
---[[
-local shader_invert = love.graphics.newShader [[
-  vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 pixel_coords) {
-    vec4 col = texture2D( texture, texture_coords );
-    return vec4(1-col.r, 1-col.g, 1-col.b, col.a);
-  }
-]]
-
 local isPressed = {}
 
 local function pair(i, j)
-  return tostring(i) .. "," .. tostring(j)
-end
-
-local function shallowCopy(t)
-  local copy = {}
-  for k, v in pairs(t) do
-    copy[k] = v
-  end
-  return copy
+  return i + 257 * j
 end
 
 function love.load()
-  mapx, mapy, _ = love.window.getMode()
-  mapx, mapy = mapx / 2, mapy / 2
+  local w, h, _ = love.window.getMode()
+  map.x, map.y = w / 2, h / 2
+  mapTransform = love.math.newTransform(
+    map.x, map.y,
+    math.pi / 4,
+    nil, nil,
+    tileSize * #map[1] / 2,
+    tileSize * #map / 2
+  )
 
   lg.setNewFont(30)
 
@@ -62,62 +40,15 @@ function love.load()
       end
     end
   end
-
-  dieRollSound = love.audio.newSource("assets/snd/dice.wav", "static")
-  for i = 1, 6 do
-    dieFaces[i] = lg.newImage("assets/img/" .. tostring(i) .. ".png")
-  end
 end
 
-function love.update(_)
+function love.update(dt)
   local obj = objectives[pair(die.y, die.x)]
   if obj == die.top then
     objectives[pair(die.y, die.x)] = nil
   end
 
-  local moved = false
-  local prev = shallowCopy(die)
-  if isPressed["w"] or isPressed["up"] then
-    die.y = die.y - 1
-    moved = true
-
-    local down = 7 - die.top
-    die.top = die.down
-    die.down = down
-  elseif isPressed["a"] or isPressed["left"] then
-    die.x = die.x - 1
-    moved = true
-
-    local right = 7 - die.top
-    die.top = die.right
-    die.right = right
-  elseif isPressed["s"] or isPressed["down"] then
-    die.y = die.y + 1
-    moved = true
-
-    local top = 7 - die.down
-    die.down = die.top
-    die.top = top
-  elseif isPressed["d"] or isPressed["right"] then
-    die.x = die.x + 1
-    moved = true
-
-    local top = 7 - die.right
-    die.right = die.top
-    die.top = top
-  end
-
-  if die.y <= 0 or die.y > #map
-      or die.x <= 0 or die.x > #map[1]
-      or map[die.y][die.x] == -1 then
-    die = prev
-    moved = false
-  end
-  if moved then
-    die.steps = die.steps + 1
-    love.audio.stop(dieRollSound)
-    love.audio.play(dieRollSound)
-  end
+  die:update(dt, map, isPressed)
 
   if lk.isDown("escape") then
     love.event.quit()
@@ -138,8 +69,7 @@ function love.draw()
   end
 
   lg.push()
-  lg.translate(mapx, 0)
-  lg.rotate(math.pi / 4)
+  lg.applyTransform(mapTransform)
 
   -- Draw isometric map at mapx,mapy
   for i = 1, #map do
@@ -152,14 +82,14 @@ function love.draw()
         lg.setColor(0, 0.5, 1, 1)
         lg.rectangle("fill", x, y, tileSize, tileSize)
       elseif tile > 0 and objectives[pair(i, j)] ~= nil then
-        lg.draw(dieFaces[tile], x, y)
+        lg.draw(Die.faces[tile], x, y)
       else
         lg.setColor(0, 1, 0.5, 1)
         lg.rectangle("fill", x, y, tileSize, tileSize)
       end
     end
   end
-
+  -- Draw map grid
   for i = 0, #map + 1 do
     lg.setColor(0, 0, 0, 1)
     lg.line(i * tileSize, 0, i * tileSize, #map * tileSize)
@@ -167,31 +97,7 @@ function love.draw()
   end
 
   -- Draw die
-  lg.setColor(1, 1, 1, 1)
-  lg.draw(
-    dieFaces[die.top],
-    (die.x - 1) * die.w - die.w / 2, (die.y - 1) * die.h - die.h / 2
-  )
-
-  lg.setColor(.85, .85, .85, 1)
-  lg.draw(
-    dieFaces[die.down],
-    (die.x - 1) * die.w - die.w / 2, (die.y - 1) * die.h + die.h / 2,
-    nil,
-    nil, 0.5,
-    nil, nil,
-    0.5, 0
-  )
-
-  lg.setColor(.75, .75, .75, 1)
-  lg.draw(
-    dieFaces[die.right],
-    die.x * die.w - 16, die.y * die.h - 48,
-    -math.pi / 2,
-    -1, 0.5,
-    32, 32,
-    0.5, 0
-  )
+  die:draw()
 
   lg.pop()
 end
